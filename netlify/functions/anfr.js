16ede0c47f63e6c7500283908eaf66b03634ad5f
nonolwdz <n.antoinemich@gmail.com>
2026-08-16 20:00:40 +0200

    Update anfr.js

--- netlify/functions/anfr.js
+++ netlify/functions/anfr.js
@@ -6,8 +6,8 @@ exports.handler = async function(event, context) {
     const lng = params.lng || '2.4357';
     const rayon = 10000; // 10 km
     
-    // URL officielle propre avec 30 résultats pour éviter tout dépassement
-    const urlANFR = `https://data.anfr.fr/d4c/api/records/1.0/search/?dataset=observatoire_2g_3g_4g&resource_id=88ef0887-6b0f-4d3f-8545-6d64c8f597da&geofilter.distance=${lat},${lng},${rayon}&rows=30`;
+    // Le "coordonnees," est obligatoire ici pour que l'API de l'État filtre autour du point GPS
+    const urlANFR = `https://data.anfr.fr/d4c/api/records/1.0/search/?dataset=observatoire_2g_3g_4g&resource_id=88ef0887-6b0f-4d3f-8545-6d64c8f597da&geofilter.distance=coordonnees,${lat},${lng},${rayon}&rows=30`;
 
     return new Promise((resolve) => {
         https.get(urlANFR, (res) => {
